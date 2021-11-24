e4d066c940207b849faea145313d01721b1b21ae
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-11-24 22:38:41 +0900

    Initial Commit

---
+++ 8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8
+//
+//  Created by Toshiyana on 2021/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
